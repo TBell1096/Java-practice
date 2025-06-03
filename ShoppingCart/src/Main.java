5938dcc7f54ddf2559285afa5d9f5fb3301148ce
TBell1096 <155848319+TBell1096@users.noreply.github.com>
2025-06-03 15:16:40 -0500

    added loops to shopping cart

--- ShoppingCart/src/Main.java
+++ ShoppingCart/src/Main.java
@@ -17,6 +17,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
+        String businessName = "Zebra Goods";
+//        String contactInformation = "512-555-5555";
+//        String itemDescription = "My product description";
+//        System.out.println("Business name: " + businessName);
+//        System.out.println("Business contact info: " + contactInformation);
+//        System.out.println("Product description: " + itemDescription);
         System.out.println("Welcome to the shopping cart app!");
         int productId = 1;
         int productCategory = 2;
@@ -24,7 +30,12 @@ public class Main {
         double productPrice = 4.99;
         int productQuantity = 78;
 
-        System.out.println("Product price: " + productPrice);
+
+        boolean isConfirmed = false;
+//        String taxExempt ="";
+//        String Shipping = "";
+//        String promoCode = "";
+//        int orderQuanity = 0;
 
         System.out.print("Are you tax-exempt? (y/n)");
         String taxExempt = console.nextLine();
@@ -38,100 +49,107 @@ public class Main {
         System.out.print("Promo code for free shipping?");
         String promo = console.nextLine();
 
+        while (!isConfirmed) {
+
+//        System.out.println("Product price: " + productPrice);
+//
+
+
         System.out.println("Details:");
         System.out.println("Tax-exempt: " + taxExempt);
         System.out.println("Shipping: " + ship);
         System.out.println("Order quantity: " + quantity);
         System.out.println("Promo code: " + promo);
 
-        String businessName = "Zebra Goods";
-        String contactInformation = "512-555-5555";
-        String itemDescription = "My product description";
-
-        System.out.println("Business name: " + businessName);
-        System.out.println("Business contact info: " + contactInformation);
-        System.out.println("Product description: " + itemDescription);
-
-
-        double taxRate = 0.07; // In percentage
-        double standardShipping = 2.00;
-        double twoDayShipping = 5.00;
-        double overnightShipping = 10.00;
-
-        double hundredDollarDiscount = 0.05;
-        double fiveHundredDollarDiscount = 0.10;
-        // total cost of product based on inventory
-        double totalCost = productCost * productQuantity;
-        System.out.println("Total Cost: " + totalCost);
-        // profit in dollars
-        double profitPerProduct = productPrice - productCost;
-        System.out.println("Profit in dollars: " + profitPerProduct);
-
-        //total potential profit
-        double totaProfit = profitPerProduct * productQuantity;
-        System.out.println("Total Profit: " + totaProfit);
-
-        //tax cost applied
-        if (taxExempt.equals("n")) {
-            totalCost = totalCost + (totalCost * totalCost);
-        }
-        System.out.println("Total w/ tax: " + totalCost);
-
-        //order total discount
-        if (totalCost >= 500) {
-            totalCost = totalCost - (totalCost * fiveHundredDollarDiscount);
-        } else if (totalCost >=  100) {
-            totalCost = totalCost - (totalCost * hundredDollarDiscount);
-        }
-        System.out.println("Total after discount: " + totalCost);
-
-        double shippingCost = 0.00;
-        switch (ship) {
-            case "standard":
-                shippingCost = standardShipping;
-                if (promo.equals("FREE")) {
-                    shippingCost = 0;
-                }
+            System.out.print("Confirm oder y/n: ");
+            String Confirmed = console.nextLine();
+            if(Confirmed.equals("y")){
                 break;
-
-            case "overnight":
-                shippingCost = overnightShipping;
-                break;
-
-            case "twoday":
-                shippingCost = twoDayShipping;
-                break;
-
-            default:
-                // invalid input
-                System.out.println("Invalid shipping type");
-
+            }else {
+                continue;
+            }
         }
-        totalCost += shippingCost;
-        System.out.println("Shipping Cost: " + shippingCost);
-        System.out.println("Final Total: " + totalCost);
-
-
-        //  Enum shipping
-        OrderStatus status = OrderStatus.PROCESSING;
-        ShippingStatus Shipping = ShippingStatus.TWO_DAY;
-        OrderStatus[] numStatus = OrderStatus.values();
-        OrderStatus pending = numStatus[0];
-        ShippingStatus[] numShipping = ShippingStatus.values();
-        ShippingStatus standard = numShipping[0];
-
-        System.out.println(ShippingStatus.STANDARD);
-        System.out.println(ShippingStatus.TWO_DAY);
-        System.out.println(ShippingStatus.OVERNIGHT);
-        OrderStatus orderStatus = OrderStatus.PROCESSING;
-        System.out.println("Order Status: " + orderStatus);
-
-        System.out.println(OrderStatus.PENDING);
-        System.out.println(OrderStatus.PROCESSING);
-        System.out.println(OrderStatus.SHIPPED);
-        System.out.println(OrderStatus.DELIVERED);
-        ShippingStatus shipStatus = ShippingStatus.TWO_DAY;
-        System.out.println("Shipping: " + shipStatus);
+//
+//
+//        double taxRate = 0.07; // In percentage
+//        double standardShipping = 2.00;
+//        double twoDayShipping = 5.00;
+//        double overnightShipping = 10.00;
+//
+//        double hundredDollarDiscount = 0.05;
+//        double fiveHundredDollarDiscount = 0.10;
+//        // total cost of product based on inventory
+//        double totalCost = productCost * productQuantity;
+//        System.out.println("Total Cost: " + totalCost);
+//        // profit in dollars
+//        double profitPerProduct = productPrice - productCost;
+//        System.out.println("Profit in dollars: " + profitPerProduct);
+//
+//        //total potential profit
+//        double totaProfit = profitPerProduct * productQuantity;
+//        System.out.println("Total Profit: " + totaProfit);
+//
+//        //tax cost applied
+//        if (taxExempt.equals("n")) {
+//            totalCost = totalCost + (totalCost * totalCost);
+//        }
+//        System.out.println("Total w/ tax: " + totalCost);
+//
+//        //order total discount
+//        if (totalCost >= 500) {
+//            totalCost = totalCost - (totalCost * fiveHundredDollarDiscount);
+//        } else if (totalCost >=  100) {
+//            totalCost = totalCost - (totalCost * hundredDollarDiscount);
+//        }
+//        System.out.println("Total after discount: " + totalCost);
+//
+//        double shippingCost = 0.00;
+//        switch (ship) {
+//            case "standard":
+//                shippingCost = standardShipping;
+//                if (promo.equals("FREE")) {
+//                    shippingCost = 0;
+//                }
+//                break;
+//
+//            case "overnight":
+//                shippingCost = overnightShipping;
+//                break;
+//
+//            case "twoday":
+//                shippingCost = twoDayShipping;
+//                break;
+//
+//            default:
+//                // invalid input
+//                System.out.println("Invalid shipping type");
+//
+//        }
+//        totalCost += shippingCost;
+//        System.out.println("Shipping Cost: " + shippingCost);
+//        System.out.println("Final Total: " + totalCost);
+//
+//
+//        //  Enum shipping
+//        OrderStatus status = OrderStatus.PROCESSING;
+//        ShippingStatus Shipping = ShippingStatus.TWO_DAY;
+//        OrderStatus[] numStatus = OrderStatus.values();
+//        OrderStatus pending = numStatus[0];
+//        ShippingStatus[] numShipping = ShippingStatus.values();
+//        ShippingStatus standard = numShipping[0];
+//
+//        System.out.println(ShippingStatus.STANDARD);
+//        System.out.println(ShippingStatus.TWO_DAY);
+//        System.out.println(ShippingStatus.OVERNIGHT);
+//        OrderStatus orderStatus = OrderStatus.PROCESSING;
+//        System.out.println("Order Status: " + orderStatus);
+//
+//        System.out.println(OrderStatus.PENDING);
+//        System.out.println(OrderStatus.PROCESSING);
+//        System.out.println(OrderStatus.SHIPPED);
+//        System.out.println(OrderStatus.DELIVERED);
+//        ShippingStatus shipStatus = ShippingStatus.TWO_DAY;
+//        System.out.println("Shipping: " + shipStatus);
 
 
     }
